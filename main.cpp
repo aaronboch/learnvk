79c8b6a6f5ba2b1d67f9bb463cacbd639fccead5
aaronboch <aaronboch8@gmail.com>
2026-06-13 11:36:21 +0200

    feat: add GLFW window creation
    References: 00_Setup/00_Base_code

--- main.cpp
+++ main.cpp
@@ -8,21 +8,43 @@ import vulkan_hpp;
 
 import std;
 
+constexpr uint32_t WIDTH = 800;
+constexpr uint32_t HEIGHT = 600;
+
 class HelloTriangleApplication {
   public:
     void run() {
-        std::cout << "Hello Triangle!" << std::endl;
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
   private:
-    void initVulkan() {}
+    GLFWwindow* window;
 
-    void mainLoop() {}
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // for now
 
-    void cleanup() {}
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan() {
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
 };
 
 int main() {
