abcc09c5079eb1cd35564ae69f75a0a05fe8f519
aaronboch <aaronboch8@gmail.com>
2026-06-13 11:54:58 +0200

    feat: creating vk instance
    reference: 00_Setup/01_Instance

--- main.cpp
+++ main.cpp
@@ -22,6 +22,8 @@ class HelloTriangleApplication {
 
   private:
     GLFWwindow* window;
+    vk::raii::Context context;
+    vk::raii::Instance instance = nullptr;
 
     void initWindow() {
         glfwInit();
@@ -32,6 +34,39 @@ class HelloTriangleApplication {
     }
 
     void initVulkan() {
+        createInstance();
+    }
+
+    void createInstance() {
+        constexpr vk::ApplicationInfo appInfo{.pApplicationName = "Hello Triangle",
+                                              .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+                                              .pEngineName = "No Engine",
+                                              .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+                                              .apiVersion = vk::ApiVersion14};
+
+        // Get the required instance extensions from GLFW.
+        uint32_t glfwExtensionCount = 0;
+        auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        // Check if the required GLFW extensions are supported by the Vulkan implementation.
+        auto extensionProperties = context.enumerateInstanceExtensionProperties();
+        for (uint32_t i = 0; i < glfwExtensionCount; ++i) {
+            if (std::ranges::none_of(extensionProperties,
+                                     [glfwExtension = glfwExtensions[i]](auto const& extensionProperty) {
+                                         return strcmp(extensionProperty.extensionName, glfwExtension) == 0;
+                                     })) {
+                throw std::runtime_error("Required GLFW extension not supported: " + std::string(glfwExtensions[i]));
+            }
+        }
+
+        vk::InstanceCreateInfo createInfo{.pApplicationInfo = &appInfo,
+                                          .enabledExtensionCount = glfwExtensionCount,
+                                          .ppEnabledExtensionNames = glfwExtensions};
+        try {
+            instance = vk::raii::Instance(context, createInfo);
+        } catch (const vk::SystemError& err) {
+            throw std::runtime_error("Failed to create Vulkan instance: " + std::string(err.what()));
+        }
     }
 
     void mainLoop() {
